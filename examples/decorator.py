8d0e68975e27ae0e3de7f14eff91427f3505cee9
Andriinef <andrii.nefedov@nure.ua>
2023-04-12 18:57:20 +0300

    10 Questions

--- examples/decorator.py
+++ examples/decorator.py
@@ -12,4 +12,20 @@ def say_hello():
     print("Hello")
 
 
-say_hello()
+def my_decorator_(func):
+    def wrapper():
+        print("Before the function is called.")
+        func()
+        print("After the function is called.")
+
+    return wrapper
+
+
+@my_decorator_
+def my_function():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    say_hello()
+    my_function()
